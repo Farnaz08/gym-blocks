31f9bcac01803cbf1a128aab2b109a0067e58079
Farnaz08 <72029515+Farnaz08@users.noreply.github.com>
2021-01-07 21:08:31 +0530

    Update blocks_env.py

--- gym_blocks/envs/blocks_env.py
+++ gym_blocks/envs/blocks_env.py
@@ -12,42 +12,41 @@ class BlocksEnv(gym.Env):
       
   def step(self, action, blocks):
      '''
-        Gives us 10 total movement options. (0,1,2,3,4,5,6,7,8,9)
+     Gives us 10 total movement options. (0,1,2,3,4,5,6,7,8,9)
         '''
-        if action == 0:
-          state= blocks[0] 
-        elif action == 1:
-          state= blocks[1]
-        elif action == 2:
-          state= blocks[2]
-        elif action == 3:
-          state= blocks[3]
-        elif action == 4:
-          state= blocks[4]
-        elif action == 5:
-          state= blocks[5]
-        elif action == 6:
-          state= blocks[6]
-        elif action == 7:
-          state= blocks[7]
-        elif action == 8:
-          state= blocks[8]
-        elif action == 9:
-          state= blocks[9]
+     if action == 0:
+       state= blocks[0] 
+     elif action == 1:
+       state= blocks[1]
+     elif action == 2:
+       state= blocks[2]
+     elif action == 3:
+       state= blocks[3]
+     elif action == 4:
+       state= blocks[4]
+     elif action == 5:
+       state= blocks[5]
+     elif action == 6:
+       state= blocks[6]
+     elif action == 7:
+       state= blocks[7]
+     elif action == 8:
+       state= blocks[8]
+     elif action == 9:
+       state= blocks[9]
           
-        if(action%2==0):
-           reward = 0
-              #print("negative reward=",reward)
-              #new_state=numberList[action]
-              count=count+1 
+     if(action%2==0):
+       reward = 0
+       #print("negative reward=",reward) 
+       #count=count+1 
              
-        elif(action%2==1):
-           reward = 100
-              #print("positive reward=",reward)
-              #count=count+1
+      elif(action%2==1):
+        reward = 100
+        #print("positive reward=",reward)
+        #count=count+1
          
-        if(reward==100):
-           done = True
+      if(reward==100):
+        done = True
         info = {}
         return state, reward, done, info
       
